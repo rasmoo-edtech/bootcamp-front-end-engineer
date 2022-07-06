0f2eefb39f947d180f418a3d9da3e25e46345139
Pedro Lacombe <pedrocarlantonio@gmail.com>
2022-07-06 13:18:40 -0300

    Exercícios de treinamento da segunda aula

---
+++ javascript/aula02/aula02.exercicios.casa.js
@@ -0,0 +1,66 @@
+// 1) Exiba no console/terminal o total de caracteres que a seguinte frase possui.
+/*
+    const oscarWildePhrase = 'Experiência é o nome que cada um dá a seus erros';
+*/
+
+
+// 2) Exiba no console/terminal o total de digitos que o seguinte número possui.
+/*
+    const price = 9449.10;
+*/
+
+
+// 3) Transforme o número de Euler para ter somente 2 casas decimais.
+/*
+    const euler = 2,71828;
+*/
+
+
+/* 4) Transforme o seguinte e-mail para letras minúsculas.
+      const email = 'fuSkiNHa@GmaiL.coM';
+*/
+
+
+/* 5) Transforme a seguinte frase para letras maiúsculas.
+        const molierePhrase = 'Não somos responsáveis apenas pelo que fazemos, mas também pelo que deixamos de fazer';
+*/
+
+
+/* 6) Verifique se a seguinte frase possui a palavra 'Equipe'.
+    const michaelJordanPhrase = 'O talento vence jogos, mas só o trabalho em equipe vence campeonatos';
+*/
+
+
+// 7) Remova todos os espaços iniciais e finais da seguinte frase e retorne o total de caracteres.
+/*
+    const vidalSassonPhrase = '  O único lugar que o sucesso vem antes do trabalho é no dicionário     ';
+*/
+
+
+// 8) Em qual indice se encontra a palavra 'excelência' na seguinte frase:
+/*
+    const aristotelesPhrase = 'Somos o que repetidamente fazemos. Portanto, a excelência não é um feito, é um hábito';
+*/
+
+
+// 9) Em qual indice se encontra a palavra 'excelência' na seguinte frase:
+/*
+    const thomasEdisonPhrase = 'Se existe uma forma de fazer melhor, descubra-a';
+*/
+
+
+// 10) Em qual indice se encontra a última ocorrência da letra s na seguinte frase:
+/*
+    const ralphWaldoPhrase = 'A confiança em si mesmo é o primeiro segredo do sucesso';
+*/
+
+
+/* 11) Transforme o seguinte texto em um array, onde cada palavra é uma posição desse array. Utilize como critério de separação o ' '.
+    const daveWeinbaumPhrase = 'Se não puder destacar-se pelo talento, vença pelo esforço';
+*/
+
+
+// 12) Remova todos os espaços em brancos do final da frase, substituia todos os espaços internos por '_' e adicione '.' ao final da frase.
+/*
+    const johnKennedyPhrase = 'Somente os que ousam errar muito podem realizar muito   ';
+*/
