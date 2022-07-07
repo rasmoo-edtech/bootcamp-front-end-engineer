10147350e1ac8bf758b9f0dc99679cc864178eab
Pedro Lacombe <pedrocarlantonio@gmail.com>
2022-07-07 19:04:27 -0300

    Resumo da terceira aula

---
+++ javascript/aula03/aula03.resumo.js
@@ -0,0 +1,28 @@
+// Manipulando arrays
+/*
+    .length; // Propriedade que retorna o número de elementos de um array
+
+    .push(); // Adiciona um ou mais elementos ao final de um array. O Array original é modificado.
+    .unshift(); // Adiciona um ou mais elementos no início de um array. O Array original é modificado.
+    .pop(); // Remove e retorna o último elemento de um array. O Array original é modificado.
+    .shift(); // Remove e retorna o primeiro elemento de um array. O Array original é modificado.
+    .splice(); // Adiciona novos elementos ou remove elementos antigos. O Array original é modificado.
+    .slice(); // Retorna uma cópia de parte de um array criado entre as posições início e fim.
+    .indexOf(); // Retorna o primeiro índice em que o elemento pode ser encontrado no array.
+    .lastIndexOf(); // Retorna o ultimo índice que um certo elemento pode ser encontrado no array.
+    .concat(); // Retorna um novo array contendo todos os arrays ou valores passados como argumento.
+    .join(); // Retorna uma string com todos os elementos do array juntos.
+    .includes(); // Retorna um boolean, true ou false, dependendo se o argumento fornecido for encontrado dentro do array.
+    .flat(); // Retorna um novo array com todos elementos sub-arrays concatenados nele.
+    .toString(); // Retorna uma string representando o array e seus elementos.
+
+    .findIndex(); // Retorna o índice no array do primeiro elemento que satisfizer a função de teste provida.
+    .find(); // Retorna o valor do primeiro elemento do array que satisfizer a função de teste provida.
+    .filter(); // Retorna um novo array com todos os elementos que passaram na função de teste provida.
+    .map(); // Retorna um novo array com base nos retornos de cada chamada feita dentro da função callback.
+    .flatMap(); // Retorna um novo array mapeando cada elemento usando uma função de mapeamento e, em seguida, nivela o resultado.
+    .reduce(); // Executa uma função callback para cada elemento do array, resultando num único valor de retorno.
+    .forEach(); // Executa uma função de callback para cada elemento de um array.
+    .some(); // Verifica se ao menos um dos elementos no array satisfizem a função de teste provida.
+    .every(); // Verifica se todos os elementos no array satisfizem a função de teste provida.
+*/
