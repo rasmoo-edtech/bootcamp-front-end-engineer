7004ff72be369561de53bf44c97b40944c56d72b
Pedro Lacombe <pedrocarlantonio@gmail.com>
2022-07-10 13:33:37 -0300

    Exercícios de treinamento aula 04

---
+++ javascript/aula04/aula04.exercicios.js
@@ -0,0 +1,85 @@
+/*
+1) Escreva um algoritmo que calcule e exiba no console/terminal a tabuada do 8, indo de 1 a 10. 
+*/
+
+
+/*
+2) Uma empresa quer verificar se um determinado empregado está apto para se aposentar. Porém, um dos 
+seguintes requisitos deve ser satisfeito:
+ - Ter no mínimo 65 anos de idade.
+ - Ter trabalhado no mínimo 30 anos.
+ - Ter no mínimo 60 anos e ter trabalhado no mínimo 25 anos.
+Com base nas informações acima, e nos dados iniciais fornecidos, escreva um algoritmo determine a idade, o tempo
+de trabalho e a mensagem 'Apto para se aposentar' ou 'Não apto para se aposentar'.
+
+const anoEmpresa = 1996;
+const anoNascimento = 1961;
+*/
+
+
+/*
+3) Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+
+---------- Álcool ----------
+- Até 20 litros, desconto de 3% por litro
+- Acima de 20 litros, desconto de 5% por litro
+
+---------- Gasolina ----------
+- Até 20 litros, desconto de 4% por litro
+- Acima de 20 litros, desconto de 6% por litro
+
+Com base nas informações acima, e nos dados iniciais fornecidos, escreva um algoritmo que
+calcule e exiba no console/terminal o valor a ser pago pelo cliente sabendo-se que o preço
+do litro da gasolina é R$ 6,49 e o preço do litro do álcool é R$ 4,52.
+
+const tipoCombustivel = 'G';
+const litrosVendidos = 35;
+*/
+
+
+/*
+4) A jornada semanal de um funcionário é de 40 horas. O funcionário que trabalhar mais
+de 40 horas receberá hora extra, cujo cálculo é o valor da hora regular com um acréscimo de 50%.
+Com base nas informações acima, e nos dados iniciais fornecidos, escreva um algoritmo que calcule
+o salário total do funcionário.
+
+const horasTrabalhadas = 48;
+const salarioHora = 8;
+*/
+
+
+
+/*
+5) Com base nos dados iniciais fornecidos, escreva um algoritmo que alcule a média aritmética e exiba
+uma mensagem dizendo se o aluno foi aprovado, reprovado ou ficou em recuperação.
+OBS: Considerar que o aluno está aprovado se sua média for maior ou igual a 7, e reprovado se sua média for menor que 5.
+
+const nota1 = 7.8;
+const nota2 = 6.7;
+*/
+
+
+/*
+6) Uma determinada loja está verificando o valor total de todas as mercadorias armazenadas em estoque. 
+Com base nos dados iniciais fornecidos, escreva um algoritmo que calcule e exiba o valor total em estoque.
+
+const totalMercadorias = 128;
+const precoUnitario = 29.90;
+*/
+
+
+/*
+7) Com base nos dados iniciais fornecidos, escreva um algoritmo que calcule e exiba a temperatura equivalente em
+Fahrenheit e Kelvin.
+
+const celsius = 42;
+
+Fórmulas de conversão:
+
+- Celsius para Fahrenheit: Tc/5 = (Tf - 32)/9
+- Celsius para kelvin: Tk = Tc + 273
+
+Tc = Temperatura em Celsius
+Tf = Temperatura em Fahrenheit
+Tk = Temperatura em Kelvin
+*/
